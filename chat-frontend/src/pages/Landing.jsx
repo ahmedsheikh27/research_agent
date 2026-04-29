f002c26635bd9b041f41d7106a4a9f9f6bc30894
ahmedsheikh27 <dev.ahmad04@gmail.com>
2026-04-30 01:16:06 +0500

    update

--- chat-frontend/src/pages/Landing.jsx
+++
@@ -1,9 +0,0 @@
-import LandingHero from '../components/LandingHero';
-
-export default function Landing() {
-  return (
-    <div className="bg-dark-900 text-slate-100 min-h-screen">
-      <LandingHero />
-    </div>
-  );
-}
